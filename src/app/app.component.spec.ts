a64c2584573345f7232a693ef27ae0ba73835e5e
AlexPs06 <luis_pesar@hotmail.com>
2019-07-25 10:36:21 -0500

    añadi un testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule, 
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
-
+ 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -31,5 +34,19 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-hosting!');
+  });
+
+
+  it('submitting a form emits a formUnidades', () => {
+    let fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    // get test component from the fixture
+    let component = fixture.componentInstance;
+    component.formUnidades.controls['Numero1'].setValue(2);
+    component.formUnidades.controls['Numero2'].setValue(2);
+    component.Suma();
+    // Now we can check to make sure the emitted value is correct
+    expect(component.Resultado).toBe(4);
+
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  formUnidades : FormGroup
   title = 'angular-hosting';
+  Resultado=0;
+  constructor(
+    private formbuilder : FormBuilder,
+    ) {
+      this.formUnidades = this.formbuilder.group({
+        Numero1 : [''],
+        Numero2 : [''],
+      })
+     }
+  ngOnInit (){
+
+     }
+  Suma(){
+    let numero = this.formUnidades.get("Numero1").value;
+    let numero2 =this.formUnidades.get("Numero2").value;
+    this.Resultado= numero+numero2
+    
+  }
 }
